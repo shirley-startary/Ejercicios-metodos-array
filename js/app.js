9670d07c8a02542cbdd53c49ad558ff86017302c
Liliwaukee <lilimmvisual@gmail.com>
2018-02-18 15:05:35 -0600

    agregando desarrollo de la función capitalize

--- js/app.js
+++ js/app.js
@@ -11,7 +11,19 @@ var capitalize = function(str) {
   //Escribe tu codigo aqui
   // No puedes utilizar directamente sobre str toUpperCase
 
+  var newArray = str.split(""); // convertimos el string en array
+
+//con map cada letra del array pasa a mayúscula
+  var capitalLetter = newArray.map(function(letter){
+     var capital = letter.toUpperCase();
+     return capital;
+   });
+
+  var againString = capitalLetter.join(""); // convertimos el array en string
+  return againString;
 }
+
+
 
 //Utiliza console.log() para visualizar el funcionamiento de tu código.
 var outputCapitalize = capitalize("whoop")
@@ -21,12 +33,12 @@ console.log(outputCapitalize); //----> "WHOOP";
 // 2. swapCase
 /*Ahora escribe una función llamada swapCase que tome una oración como string y
   retorne el string alternando una palabra en mayúsculas y otra en minúsculas
-  ejem: swapCase('hey gurl, lets javascript together sometime') ---> "HEY gurl, LETS javascript TOGETHER sometime" 
+  ejem: swapCase('hey gurl, lets javascript together sometime') ---> "HEY gurl, LETS javascript TOGETHER sometime"
   NOTA: Debes hacer uso de la funcion capitalize();*/
 
 var swapCase = function(str) {
   // Escribe tu codigo aquí
-  
+
 }
 
 var outputSwapCase = swapCase("hey gurl, lets javascript together sometime")
@@ -81,7 +93,6 @@ console.log(outputOddNumbers); // ---> [1, 3, 5, 7, 9]
   arreglo de dos elementos, el primer elemento será la suma de los números pares y el
   segundo elemento la suma de los números impares
   ejem: reducer([1,2,3,4,5,6,7,8,9]) ---> [20, 25]
-
 Nota: Debes de hacer uso de las funciones de evenNumbers() y oddNumbers.*/
 
 var reducer = function() {
@@ -97,13 +108,13 @@ console.log(outputReducer); // ---> [ 20, 25 ]
 /*Manipula el siguiente array y devuelve un nuevo array que contenga los strings 'javascript',
   debes concatenar dos difrentes métodos para arrays.
   Apartir de este ejercicio te toca darle nombre y forma a tus funciones y sus respectivos outputs.
-  ejem. output --> ['javascript', 'javascript', 'javascript']*/ 
+  ejem. output --> ['javascript', 'javascript', 'javascript']*/
 
 
 
 var persons = [
-  {id : 1, name : "John", tags : "javascript"}, 
-  {id : 2, name : "Alice", tags : "javascript"}, 
+  {id : 1, name : "John", tags : "javascript"},
+  {id : 2, name : "Alice", tags : "javascript"},
   {id : 3, name : "Roger", tags : "java"},
   {id : 4, name : "Adam", tags : "javascript"},
   {id : 5, name : "Alex", tags : "java"}
@@ -118,16 +129,16 @@ var paintPersons = function() {
 }
 
 var outputPaintPerson = paintPersons(persons);
-  
-// 9. Total de edad en 'años perro' 
+
+// 9. Total de edad en 'años perro'
 /*Considera la siguiente variables data. Nosotros tenmos un arreglo de objetos, cada objeto representa
   una mascota. Las mascotas tienen un nombre, una edad y un tipo.
-  Instrucciones: 
+  Instrucciones:
   1. Selecciona solo a los perros.
   2. Transforma su edad en años perros (multiplica cada una por siete).
   3. Suma el resultado.
-  
-  ejem. output --> 84 
+
+  ejem. output --> 84
 */
 
 var data = [
@@ -153,7 +164,7 @@ var data = [
   },
 ];
 
-// 10. Render in DOM 
+// 10. Render in DOM
 /*Usando la data anterior y alguno de los métodos, pinta en el index.html a través del DOM
  cada una de las mascotas junto con todas su propiedades*/
 
@@ -168,6 +179,3 @@ var outputPaintPets = paintPets(data);
   ejem. output --> ['Apple', 'Banana', 'Kiwi', 'Orange'];*/
 
 var fruits = ['Banana', 'Orange', 'Apple', 'Kiwi'];
-
-
-
