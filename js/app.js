67a8aa1094fa79e4942eba4c4c93087e40ed0388
Shirley Silvana <shirley.startary@gmail.com>
2018-03-01 15:21:54 -0600

    Agregando solucionario

--- js/app.js
+++ js/app.js
@@ -8,39 +8,37 @@
  * ejem: capitalize("whoop") --->  "WHOOP" */
 
 var capitalize = function(str) {
-  //Escribe tu codigo aqui 
+  //Escribe tu codigo aqui
   // No puedes utilizar directamente sobre str toUpperCase
-  var array = str.split("");
-  var newArray = array.map(function(item){
-    // console.log(item);
-    return item.toUpperCase();
-  });
-  var newString = newArray.join("");
+  const arrayLetters = str.split("");
+  const newArrayLetter = arrayLetters.map(item => item.toUpperCase());
+  let newString = newArrayLetter.join("");
   return newString;
 }
 
 //Utiliza console.log() para visualizar el funcionamiento de tu código.
 var outputCapitalize = capitalize("whoop")
-// console.log(outputCapitalize); //----> "WHOOP";
+console.log(outputCapitalize); //----> "WHOOP";
 
 
 // 2. swapCase
 /*Ahora escribe una función llamada swapCase que tome una oración como string y
   retorne el string alternando una palabra en mayúsculas y otra en minúsculas
-  ejem: swapCase('hey gurl, lets javascript together sometime') ---> "HEY gurl, LETS javascript TOGETHER sometime" 
+  ejem: swapCase('hey gurl, lets javascript together sometime') ---> "HEY gurl, LETS javascript TOGETHER sometime"
   NOTA: Debes hacer uso de la funcion capitalize();*/
 
-var swapCase = function(str) {  
+var swapCase = function(str) {
   // Escribe tu codigo aqui
-  var array = str.split(" ");
-  var newArray = array.map(function(element, index){
-    // console.log(element, index);
+  const arrayWords = str.split(" ");
+
+  const newArrayWord = arrayWords.map((item, index) => {
     if (index % 2 === 0) {
-      return capitalize(element);
+      return capitalize(item);
     }
-    return element;
+    return item;
   });
-  var newString = newArray.join(" ");
+
+  let newString = newArrayWord.join(" ");
   return newString;
 }
 
@@ -59,6 +57,13 @@ console.log(outputSwapCase); //---> "HEY gurl, LETS javascript TOGETHER sometime
 
 var shiftLetters = function(str) {
   //Escribe tu codigo aquí
+  const arrayString = str.split("");
+
+  const arrayCode = arrayString.map(item => {
+    return String.fromCharCode(item.charCodeAt() + 1);
+  });
+
+  return arrayCode.join("");
 }
 
 var outputShiftLetters = shiftLetters('hello');
@@ -73,6 +78,8 @@ var numberArray = [1,2,3,4,5,6,7,8,9,10];
 
 var evenNumbers = function(array) {
   //Escribe tu codigo aquí.
+  const newArray = array.filter(item => item % 2 === 0);
+  return newArray;
 };
 
 var outputEvenNumbers = evenNumbers(numberArray);
@@ -85,6 +92,8 @@ console.log(outputEvenNumbers); // ---> [2, 4, 6, 8, 10]
 
 var oddNumbers = function(array) {
   //Escribe tu codigo aquí
+  const newArray = array.filter(item => item % 2 !== 0);
+  return newArray;
 };
 
 var outputOddNumbers = oddNumbers(numberArray);
@@ -99,12 +108,15 @@ console.log(outputOddNumbers); // ---> [1, 3, 5, 7, 9]
 
 Nota: Debes de hacer uso de las funciones de evenNumbers() y oddNumbers.*/
 
-var reducer = function() {
+var reducer = function(array) {
   //Escribe tu codigo aquí
-
+  const sumEvenNumber = evenNumbers(array).reduce((prev, inicial) => prev + inicial);
+  const sumOddNumber = oddNumbers(array).reduce((prev, inicial) => prev + inicial);
+  const newArray = [sumEvenNumber, sumOddNumber];
+  return newArray;
 }
 
-var outputReducer = reducer([1,2,3,4,5,6,7,8,9]);
+var outputReducer = reducer([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 console.log(outputReducer); // ---> [ 20, 25 ]
 
 
@@ -112,40 +124,68 @@ console.log(outputReducer); // ---> [ 20, 25 ]
 /*Manipula el siguiente array y devuelve un nuevo array que contenga los strings 'javascript',
   debes concatenar dos difrentes métodos para arrays.
   Apartir de este ejercicio te toca darle nombre y forma a tus funciones y sus respectivos outputs.
-  ejem. output --> ['javascript', 'javascript', 'javascript']*/ 
+  ejem. output --> ['javascript', 'javascript', 'javascript']*/
 
 
 
 var persons = [
-  {id : 1, name : "John", tags : "javascript"}, 
-  {id : 2, name : "Alice", tags : "javascript"}, 
+  {id : 1, name : "John", tags : "javascript"},
+  {id : 2, name : "Alice", tags : "javascript"},
   {id : 3, name : "Roger", tags : "java"},
   {id : 4, name : "Adam", tags : "javascript"},
   {id : 5, name : "Alex", tags : "java"}
 ];
 
+var filtarTags = function (array) {
+  var newArray = array.filter(function(elemento){
+    return elemento.tags == "javascript";
+  })
+  var newArrayTags = newArray.map(function(elemento){
+    return elemento.tags;
+  });
+  return newArrayTags;
+}
+
+var outputFiltrarTags = filtarTags(persons);
+console.log(outputFiltrarTags);
+
+
 // 8. Render in DOM
 /*Usando la data anterior y alguno de los métodos, pinta en el index.html a través del DOM
  cada una de las personas y todas sus propiedades */
 
-var paintPersons = function() {
-
+var paintPersons = function(array) {
+  var container = document.getElementById("container");
+  array.forEach(function(elemento){
+    var person = document.createElement("div");
+    var nombre = document.createElement("h3");
+    var id = document.createElement("p");
+    var tags = document.createElement("p");
+    nombre.innerText = elemento.name;
+    id.innerText = elemento.id;
+    tags.innerText = elemento.tags;
+    person.appendChild(nombre);
+    person.appendChild(id);
+    person.appendChild(tags);
+    container.appendChild(person);
+  });
 }
 
 var outputPaintPerson = paintPersons(persons);
-  
-// 9. Total de edad en 'años perro' 
+// console.log(outputPaintPerson);
+
+// 9. Total de edad en 'años perro'
 /*Considera la siguiente variables data. Nosotros tenmos un arreglo de objetos, cada objeto representa
   una mascota. Las mascotas tienen un nombre, una edad y un tipo.
-  Instrucciones: 
+  Instrucciones:
   1. Selecciona solo a los perros.
   2. Transforma su edad en años perros (multiplica cada una por siete).
   3. Suma el resultado.
-  
-  ejem. output --> 84 
+
+  ejem. output --> 84
 */
 
-var data = [
+const data = [
   {
     name: 'Butters',
     age: 3,
@@ -168,7 +208,19 @@ var data = [
   },
 ];
 
-// 10. Render in DOM 
+const dogs = array => {
+  const newArray = array.filter((elemento) => { return elemento.type === 'dog' });
+
+  const ageDogs = newArray.map((item)=>{return item.age * 7 }).reduce((prev, inicial)=>{ return prev + inicial});
+  // const sumaAgeDogs = ageDogs.reduce((valorAnt, valorInicial) => {
+  //   console.log(valorAnt + valorInicial);
+  //   return valorAnt + valorInicial;
+  // });
+  return ageDogs;
+}
+
+console.log(dogs(data));
+// 10. Render in DOM
 /*Usando la data anterior y alguno de los métodos, pinta en el index.html a través del DOM
  cada una de las mascotas junto con todas su propiedades*/
 
@@ -183,5 +235,3 @@ var outputPaintPets = paintPets(data);
   ejem. output --> ['Apple', 'Banana', 'Kiwi', 'Orange'];*/
 
 var fruits = ['Banana', 'Orange', 'Apple', 'Kiwi'];
-
-
